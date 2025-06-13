05403ffcc523d1293cf12fb1e26f1631d5286aa0
Jules-Elvin Andrade <juleselvandrade@gmail.com>
2025-06-12 20:32:23 -0700

    combined q and a randomized picking w/ flashcard styling worked

--- src/App.jsx
+++ src/App.jsx
@@ -4,35 +4,50 @@ import { useState } from 'react';
 const App = () => {
 
   const QandA = {
-    "introQ": "introA",
-    "bazinga1": "badingi1",
-    "bazinga2": "badingi2",
-    "bazinga3": "badingi3",
-    "bazinga4": "badingi4",
-    "bazinga5": "badingi5",
+    "question1": "badingi1",
+    "question2": "badingi2",
+    "question3": "badingi3",
+    "question4": "badingi4",
+    "question5": "badingi5",
+    "question6": "badingi6",
+    "question7": "badingi7",
+    "question8": "badingi8",
+    "question9": "badingi9",
+    "question10": "badingi10",
   };
 
-  const [currentQuestion, setCurrentQuestion] = useState(""); // by default, this should be the first key/value pair
-  const [currentAnswer, setCurrentAnswer] = useState(""); // by default, this should be the first key/value pair
+  // that was over engineered i'm just gonna type it in myself bruh
+  const [currentQuestion, setCurrentQuestion] = useState("introQ"); 
+  const [currentAnswer, setCurrentAnswer] = useState("introA"); 
+
+  const questions = Object.keys(QandA);
 
   const handleFlashcard = () => {
-    // generate random index
-    // use setcurrentquestion to set currentquestion to the random key in QandA
-    // use setcurrentanswer to set currentanswer to the corresponding answer for the random question
-    // remove the introQ + introA key/value pair from the dictionary
+    let random = Math.floor(Math.random() * questions.length);
+    setCurrentQuestion(questions[random]);
+    setCurrentAnswer(QandA[questions[random]]);
   }
 
   return (
     <div className="container">
-      <h1 className="headers"> Basic Japanese! 🇯🇵 </h1>
-      <h2 className="headers"> Departing to Japan soon? Or just curious about the language of Japan? Test your knowledge here! </h2>
-      <h4 className="headers"> Number of cards: </h4>
+      <div className="header-container">
+        <h1> Basic Japanese! 🇯🇵 </h1>
+        <h2> Departing to Japan soon? Or just curious about the language of Japan? Test your knowledge here! </h2>
+        <h4> Number of cards: {questions.length} </h4>
+      </div>
 
       <div className="flashcard">
-        <h3> CURRENT QUESTION PLACEHOLDER: {currentQuestion} </h3>
-        <h3> CURRENT ANSWER PLACEHOLDER: {currentAnswer} </h3>
-        <button> test button to switch between questions </button> {/* onclick calls handleflashcard, should remove the placeholder question + answer */}
+        <div class="flashcard-inner">
+          <div className="flashcard-front">
+            <h3> CURRENT QUESTION PLACEHOLDER: {currentQuestion} </h3>
+          </div>
+          <div className="flashcard-back">
+            <h3> CURRENT ANSWER PLACEHOLDER: {currentAnswer} </h3>
+          </div>
+        </div>
       </div>
+
+      <button onClick={handleFlashcard}> &rarr; </button> 
 
     </div>
   )
